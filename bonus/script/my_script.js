0faa8e15dffc0cda4d9420418bd516402e1b9f7d
EmanuelaPau <uapame@gmail.com>
2023-05-23 17:22:57 +0200

    Add function to create grid

--- bonus/script/my_script.js
+++ bonus/script/my_script.js
@@ -9,7 +9,6 @@ console.log(`My grid element is ${myGrid}`);
  * @returns 100 cell elements
  */
 
-// Questo non funzionera' per il bonus
 function createCellElement(tagName, className) {
 
     const myCell = document.createElement(tagName);
@@ -21,28 +20,38 @@ const playButton = document.querySelector('.playbutton');
 
 const difficultySelector = document.querySelector('select');
 
+/**
+ * Function that creates a grid with cells
+ * @param numCells the number of cells in the grid
+ */
+
+function createGrid(numCells) {
+
+    myGrid.classList.add('grid-border');
+    for (i = 1; i <= numCells; i++) {
+        const appendMyCell = createCellElement("div", "cell");
+        appendMyCell.innerHTML = '<p>' + i + '</p>'
+
+        appendMyCell.addEventListener('click', function () {
+            appendMyCell.classList.toggle('selected');
+        })
+        myGrid.appendChild(appendMyCell);
+    }
+}
+
 playButton.addEventListener('click', function () {
     // reset 
     myGrid.innerHTML = "";
-
     if (difficultySelector.value == '1') {
         alert('hard');
-
-        myGrid.classList.add('grid-border');
-        for (i = 1; i <= 100; i++) {
-            const appendMyCell = createCellElement("div", "cell");
-            appendMyCell.innerHTML = '<p>' + i + '</p>'
-
-            appendMyCell.addEventListener('click', function () {
-                appendMyCell.classList.toggle('selected');
-            })
-            myGrid.appendChild(appendMyCell);
-        }
+        createGrid('100')
 
     } else if (difficultySelector.value == '2') {
         alert('medium');
+        createGrid('81')
     } else {
         alert('easy');
+        createGrid('49')
     }
 })
 
